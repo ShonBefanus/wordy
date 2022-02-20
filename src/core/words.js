0a3e871bde2ae5dbe3e597bbc835059b3245e3cc
ShonBefanus <magic34@gmx.com>
2022-02-20 11:09:43 -0500

    update words to 9169

--- src/core/words.js
+++ src/core/words.js
@@ -1,5 +1,6 @@
-import  weight1 from './wordsByWeightObj';
-import  words1 from './wordsSorted';
+import weight1 from './wordsByWeightObj';
+import words1 from './wordsSorted';
+//const words = (() => {const t= weight1.map(({word}) => word); return ()=>t})();
 
 export const weight = weight1;
 export const words = words1;
